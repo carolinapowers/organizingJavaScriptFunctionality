c01a3d827886d3376c0df021e691cd25af79dd9c
cpowers <cpowers@DELOITTE.com>
2016-05-13 14:26:28 -0400

    refactoring into modules patterns

---
+++ js/header.js
@@ -0,0 +1,28 @@
+// put event handlers for header links here
+
+var Header = (function () {
+    var $modal; 
+    
+    function headerLinkClicks(evt) {
+        evt.preventDefault();
+        evt.stopImmediatePropagation();
+        evt.stopPropagation();
+        var url = $(evt.target).attr("href");
+
+        $.ajax(url, {dataType: "text"}).then(function(contents) {
+            $modal.html(contents).show();
+        }); 
+    }
+    function init() {
+        $modal = $("[rel='js-modal']");
+        $("[rel='js-controls']").on("click", "[rel*='js-']",  headerLinkClicks);
+    }
+    
+    return {
+        init: init
+    }
+    
+})();
+
+  
+$(document).ready(Header.init);
