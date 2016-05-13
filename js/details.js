5a6e4d24c193b16e68138ac76c3b88934a6f842c
cpowers <cpowers@DELOITTE.com>
2016-05-13 14:34:12 -0400

    modularized details.js

--- js/details.js
+++ js/details.js
@@ -1,26 +1,22 @@
-$(document).ready(function(){
 
-	var $items = $("[rel=js-carousel] > [rel=js-content] > [rel=js-items]");
-	var $content = $("[rel=js-details]");
-    
-    $items.on("click", "[rel^='js-item-']", personClicked);
-    
+var Details = (function () {
+    var $items, $content;
+    function init() {
+        $items = $("[rel=js-carousel] > [rel=js-content] > [rel=js-items]");
+        $content = $("[rel=js-details]");
+        $items.on("click", "[rel^='js-item-']", personClicked);
+    }
     function personClicked (evt) {
         var id = $(evt.target).attr("rel").replace(/^.*(\d+)$/, "$1");
-        console.log(id);
-        
         $.ajax("details/" + id + ".html", {dataType:"text"}).then(function (contents) {
             $content.html(contents);
         })
     }
+   
+    return{ 
+        init: init
+    }
+    
+})(); 
 
-	// on click of a carousel item, do an Ajax request for
-	// the "details/2.html" (or whatever) file for the person
-	// clicked, and load those contents into the `$content` div.
-
-	// hint: you will probably want to use "event propagation"
-	// (aka "event delegation"), by attaching a single event
-	// handler the `$content` element rather than individual
-	// event handlers to each item in the carousel.
-
-});
+$(document).ready(Details.init);
